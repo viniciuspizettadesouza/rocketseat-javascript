339976d4eb608f08387ff87cba7c4621b89cdc4f
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-25 02:18:58 -0300

    axios syntax

--- 4-JS-Assíncrono/main.js
+++ 4-JS-Assíncrono/main.js
@@ -1,25 +1,4 @@
-var minhaPromise = function () {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest()
-        xhr.open('GET', 'https://api.github.com/users/viniciuspzt')
-        xhr.send(null)
-
-        xhr.onreadystatechange = function () {
-
-            // 4 é a variavel de resposta da requisição
-
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText))
-                } else {
-                    reject('Erro na requisição')
-                }
-            }
-        }
-    })
-}
-
-minhaPromise()
+axios.get('https://api.github.com/users/viniciuspzt')
     .then(function (response) {
         console.log(response)
     })
