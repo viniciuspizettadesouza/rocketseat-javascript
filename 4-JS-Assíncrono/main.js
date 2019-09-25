dda105166f678ea143b33999bbb77583358dc65f
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-25 01:54:48 -0300

    requisições ajax

--- 4-JS-Assíncrono/main.js
+++ 4-JS-Assíncrono/main.js
@@ -1,2 +1,13 @@
 var xhr = new XMLHttpRequest()
 
+xhr.open('GET', 'https://api.github.com/users/viniciuspzt')
+xhr.send(null)
+
+xhr.onreadystatechange = function () {
+
+    // 4 é a variavel de resposta da requisição
+
+    if (xhr.readyState === 4) {
+        console.log(JSON.parse(xhr.responseText))
+    }
+}
